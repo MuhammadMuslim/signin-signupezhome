dba26d75162bfe60b04aca49db3c708b0de0e801
ms111213 <muslimalfathih19@gmail.com>
2018-08-30 16:13:09 +0700

    signin_signup

---
+++ app/src/main/java/itam/ezhome/PHP Files/DatabaseConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "localhost";
+
+//Define your database username here.
+$HostUser = "id6957285_muslim";
+
+//Define your database password here.
+$HostPass = "ms111213";
+
+//Define your database name here.
+$DatabaseName = "id6957285_ezhome";
+
+?>
